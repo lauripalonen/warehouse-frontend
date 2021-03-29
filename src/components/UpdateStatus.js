90ec6553f29d262872765d36cc341b7d394eb644
lauripalonen <palonen.lauri@gmail.com>
2021-03-29 21:37:47 +0300

    create div for update status

---
+++ src/components/UpdateStatus.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdateStatus = ({ status }) => {
+  return (
+    <div className={'update-status-block'}>
+      {status}
+    </div>
+  )
+}
+
+export default UpdateStatus
